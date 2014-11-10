12c4f297e1c537a9c58101473598aaf1493c9a2f
Shing Lyu <shing.lyu@gmail.com>
2014-11-10 13:30:57 +0800

    Permlink expeirment failed

--- data/overridesearch.js
+++ data/overridesearch.js
@@ -1,3 +1,38 @@
-var mysearch = document.createElement('input')
-mysearch.innterHTML = "<input type='text' id='mysearch' />"
-document.body.appendChild(mysearch);
+//hide the original
+var origSearch = document.getElementById('text-filter')
+//origSearch.style.display= 'none';
+
+var mysearch = document.createElement('input');
+mysearch.setAttribute('id', 'mysearch')
+mysearch.setAttribute('type', 'text')
+mysearch.setAttribute('placeholder', 'Draft area (NO auto-completion)(press TAB when done)')
+
+var searchdiv = document.getElementsByClassName('textual')[0];
+//searchdiv.appendChild(mysearch);
+searchdiv.insertBefore(mysearch, origSearch);
+//searchdiv.innerHTML = "<input type='text' id='mysearch' placeholder='moztraphelper enhanced search'/>" + searchdiv.innerHTML;
+
+var mySearch = document.getElementById('mysearch');
+
+
+var timerID = 0;
+var timeout = 0;
+var minStrLen = 0
+
+mySearch.oninput = function(){
+  clearTimeout(timerID);
+  if (mySearch.value.length > minStrLen){
+    timerID = setTimeout(function(){
+      search()
+    }, timeout)
+  }
+};
+
+function search(){
+  //origSearch.attr('value', mySearch.value);
+  $('#text-filter').val(mySearch.value)
+  //$('#text-filter').focus()
+  //mysearch.focus();
+}
+
+mysearch.focus()
