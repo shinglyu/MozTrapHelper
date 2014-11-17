9f294bc1a3f8eca1e880f5d20529a52a68155c5a
Shing Lyu <shing.lyu@gmail.com>
2014-11-17 15:07:37 +0800

    Clenaup

--- data/jsperf.js
+++ data/jsperf.js
@@ -1,35 +1,5 @@
-//console.log(unsafeWindow.MT);
-/*
-function onJqueryLoad(callback){
-  if (typeof unsafeWindow.$ == 'undefined'){
-    console.log('jQuery not ready')
-    setTimeout(function(){onJqueryLoad(callback)}, 10)
-  }
-  else{
-    console.log('jQuery  READY!')
-    unsafeWindow.$.fn.cusomeAutocomplete = null;
-    callback()
-  }
-}
-
-function onScriptLoad(){
-  if (typeof unsafeWindow.$.fn.customAutocomplete == 'undefined'){
-    console.log('script not ready')
-    setTimeout(onScriptLoad, 100)
-  }
-  else{
-    console.log('script READY!')
-    unsafeWindow.$.fn.cusomeAutocomplete = null;
-  }
-}
-
-onJqueryLoad(onScriptLoad)
-*/
-//unsafeWindow.MT = null;
-
-//var ich = unsafeWindow.ich;
+//override default autocomplete
 unsafeWindow.$('#text-filter').unbind()
-//exportFunction($.fn.customAutocomplete, unsafeWindow.$.fn, {defineAs: 'customAutocomplete'});
 
 var autocomplete_suggestion_template = '\
 {{# suggestions }}\
